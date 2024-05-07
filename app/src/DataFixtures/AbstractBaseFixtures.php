6e942a63f11c6b0db54344c9a90b9cbd25bf3517
Justyna Jawniak <jawniakjustyna@gmail.com>
2024-05-07 20:35:16 +0200

    Category and task related fixtures

--- app/src/DataFixtures/AbstractBaseFixtures.php
+++ app/src/DataFixtures/AbstractBaseFixtures.php
@@ -9,9 +9,11 @@ use Faker\Generator;
 
 abstract class AbstractBaseFixtures extends Fixture
 {
-    protected Generator $faker;
+    protected ?Generator $faker = null;
 
-    protected ObjectManager $manager;
+    protected ?ObjectManager $manager = null;
+
+    private array $referencesIndex = [];
 
     public function load(ObjectManager $manager) :void
     {
@@ -21,4 +23,57 @@ abstract class AbstractBaseFixtures extends Fixture
     }
 
     abstract protected function loadData() :void;
+
+    protected function createMany(int $count, string $groupName, callable $factory) : void
+    {
+        for($i=0; $i<$count; ++$i)
+        {
+            $entity=$factory($i);
+
+            if(null === $entity)
+            {
+                throw new \LogicException('Did you forget to return the entity object from your callback to BaseFixture::createMany()?');
+            }
+
+            $this->manager->persist($entity);
+
+            $this->addReference(sprintf('%s_%d', $groupName, $i), $entity);
+        }
+    }
+
+    protected function getRandomReference(string $groupName): object
+    {
+        if(!isset($this->referencesIndex[$groupName]))
+        {
+            $this->referencesIndex[$groupName] = [];
+
+            foreach(array_keys($this->referenceRepository->getReferences()) as $key)
+            {
+                if(str_starts_with((string)$key, $groupName.'_'))
+                {
+                    $this->referencesIndex[$groupName][] = $key;
+                }
+            }
+        }
+
+        if(empty($this->referencesIndex[$groupName]))
+        {
+            throw new \InvalidArgumentException(sprintf('Did not find any references saved with the group name "%s"', $groupName));
+        }
+
+        $randomReferenceKey = (string)$this->faker->randomElement($this->referencesIndex[$groupName]);
+
+        return $this->getReference($randomReferenceKey);
+    }
+
+    protected function getRandomReferences(string $groupName, int $count): array
+    {
+        $references = [];
+        while(count($references) < $count)
+        {
+            $references[] = $this->getRandomReference($groupName);
+        }
+
+        return $references;
+    }
 }
--- app/src/DataFixtures/TaskFixtures.php
+++ app/src/DataFixtures/TaskFixtures.php
@@ -6,31 +6,48 @@
 namespace App\DataFixtures;
 
 use App\Entity\Task;
-use DateTimeImmutable;
-use Doctrine\Persistence\ObjectManager;
+use App\Entity\Category;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 /**
  * Class TaskFixtures.
  */
-class TaskFixtures extends AbstractBaseFixtures
+class TaskFixtures extends AbstractBaseFixtures implements DependentFixtureInterface
 {
     /**
      * Load data.
+     *
+     * @psalm-suppress PossiblyNullPropertyFetch
+     * @psalm-suppress PossiblyNullReference
+     * @psalm-suppress UnusedClosureParam
      */
     public function loadData(): void
     {
-        for ($i = 0; $i < 100; ++$i) {
+        if(null===$this->manager || null===$this->faker) {
+            return;
+        }
+
+        $this->createMany(100, 'tasks', function (int $i){
             $task = new Task();
             $task->setTitle($this->faker->sentence);
             $task->setCreatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
             );
             $task->setUpdatedAt(
-                DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
             );
-            $this->manager->persist($task);
-        }
+            /** @var Category $category */
+            $category=$this->getRandomReference('categories');
+            $task->setCategory($category);
+
+            return $task;
+        });
 
         $this->manager->flush();
+    }
+
+    public function getDependencies() : array
+    {
+        return [CategoryFixtures::class];
     }
 }
