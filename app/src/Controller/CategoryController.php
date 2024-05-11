83ed1450f4d49fb34fa64bc7f3aa80b1c82a9ab6
Justyna Jawniak <jawniakjustyna@gmail.com>
2024-05-11 20:11:53 +0200

    Category service layer

--- app/src/Controller/CategoryController.php
+++ app/src/Controller/CategoryController.php
@@ -3,9 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-use App\Repository\CategoryRepository;
 use App\Repository\TaskRepository;
-use Knp\Component\Pager\PaginatorInterface;
+use App\Service\CategoryServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -14,22 +13,21 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
+    public function __construct(private readonly CategoryServiceInterface $categoryService, private readonly TaskRepository $taskRepository)
+    {}
+
     #[Route(name: 'category_index', methods: 'GET')]
-    public function index(CategoryRepository $categoryRepository, PaginatorInterface $paginator, #[MapQueryParameter] int $page = 1): Response
+    public function index(#[MapQueryParameter] int $page = 1): Response
     {
-        $pagination = $paginator->paginate(
-            $categoryRepository->queryAll(),
-            $page,
-            CategoryRepository::PAGINATOR_ITEMS_PER_PAGE
-        );
+        $pagination = $this->categoryService->getPaginatedList($page);
 
         return $this->render('category/index.html.twig', ['pagination' => $pagination]);
     }
 
     #[Route('/{id}', name: 'category_show', methods: 'GET')]
-    public function show(Category $category, TaskRepository $taskRepository): Response
+    public function show(Category $category): Response
     {
-        $tasks = $taskRepository->findTasksByCategory($category);
+        $tasks = $this->taskRepository->findTasksByCategory($category);
 
         return $this->render('category/show.html.twig', ['category' => $category, 'tasks' => $tasks]);
     }
---
+++ app/src/Service/CategoryService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\CategoryRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+class CategoryService implements CategoryServiceInterface
+{
+    private const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    public function __construct(private readonly CategoryRepository $categoryRepository, private readonly PaginatorInterface $paginator)
+    {
+    }
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface<string, mixed> Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->categoryRepository->queryAll(),
+            $page,
+            self::PAGINATOR_ITEMS_PER_PAGE
+        );
+    }
+}
