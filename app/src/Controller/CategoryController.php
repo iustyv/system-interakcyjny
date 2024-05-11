621b1a96bcd77f8aa9f4abb2daec12346584dc45
Justyna Jawniak <jawniakjustyna@gmail.com>
2024-05-11 19:32:40 +0200

    Category pagination

--- app/src/Controller/CategoryController.php
+++ app/src/Controller/CategoryController.php
@@ -5,19 +5,25 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\TaskRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
     #[Route(name: 'category_index', methods: 'GET')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, PaginatorInterface $paginator, #[MapQueryParameter] int $page = 1): Response
     {
-        $categories = $categoryRepository->queryAll()->getQuery()->getResult();
+        $pagination = $paginator->paginate(
+            $categoryRepository->queryAll(),
+            $page,
+            CategoryRepository::PAGINATOR_ITEMS_PER_PAGE
+        );
 
-        return $this->render('category/index.html.twig', ['categories' => $categories]);
+        return $this->render('category/index.html.twig', ['pagination' => $pagination]);
     }
 
     #[Route('/{id}', name: 'category_show', methods: 'GET')]
